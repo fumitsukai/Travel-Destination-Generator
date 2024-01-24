a03ad3cbc11bc3b9f678224ca5623c52cf9e04de
Mohammed Rahman <mohammed.rahman676@gmail.com>
2024-01-24 13:18:40 +0000

    added function to format date for departure

--- assets/js/flight.js
+++ assets/js/flight.js
@@ -81,6 +81,15 @@ function submitForm() {
     console.log('Form Submitted');
 }
 
+ // Function to format departure date
+ function formatDepartureDate(rawDate) {
+    const date = new Date(rawDate);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
+
 });
 
 
