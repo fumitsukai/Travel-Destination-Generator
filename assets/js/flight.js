17dd436b3ceca3101367e48d5ac11d141679cbc8
Mohammed Rahman <mohammed.rahman676@gmail.com>
2024-01-24 12:43:11 +0000

    added api key and event handler for airport code user input

--- assets/js/flight.js
+++ assets/js/flight.js
@@ -1,1 +1,10 @@
-// Creation on JavaScript File for Flight API
+$(document).ready(function () {
+    const apiKey = "65b102486c73229c59781bc2";
+
+    // Event handler for the 'arrival_airport_code' input
+    $('#arrival_airport_code').on('input', function () {
+        let inputValue = $(this).val();
+        inputValue = inputValue.replace(/[^A-Z]/g, '');
+        inputValue = inputValue.slice(0, 3);
+        $(this).val(inputValue);
+    }); }
