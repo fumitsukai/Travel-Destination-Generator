d19b7bcc8cd2abda48b7b652f402a7c9a7f95ced
Alexandru Pandelea <alexandrupandelea@Alexandrus-MacBook-Pro.local>
2024-01-24 19:49:32 +0000

    adds ids to localstorage when you click save

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,33 @@
 const searchBtn = $('#search-btn');
 const formEl = $('.questions-container');
 const carousel = $('.carousel-container-main');
-const mainContainer = $('.main-container');
+const locationInput = $('#formGroupExampleInput');
+
+let interest = "architecture";
+//$('form-check-input:checked')
+
 
 //search button event listener
+searchBtn.on('click', function () {
+    gridContainer.empty();
 
+    const discoverCity = $('<h1>')
+    .addClass('destination-header fw-semibold pb-3')
+    .text(`Discover ${locationInput.val()}`);
 
-searchBtn.on('click', function() {
-    mainContainer.empty();
-    searchArea(area,interest);
+    gridContainer.append(discoverCity);
+
+    searchArea(locationInput.val(), interest);
+})
+
+//function to save to local storage
+
+$('body').on('click', '.saveBtn', function() {
+     var thisBtn = $(this);
+     const fav = JSON.parse(localStorage.getItem('id')) || [];
+     const save = {
+        id: thisBtn.data('id')
+    }
+     fav.push(save);
+     localStorage.setItem('id', JSON.stringify(fav));
 })
